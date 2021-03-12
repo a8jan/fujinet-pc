d39851313aa17e7f365dd24a61e23c3671f9df14
Jan Krupa <apc.atari@gmail.com>
2021-03-12 15:02:04 +0100

    making git happy

--- lib/http/httpServiceConfigurator.h
+++ lib/http/httpServiceConfigurator.h
@@ -19,7 +19,7 @@ class fnHttpServiceConfigurator
     static void config_rotation_sounds(std::string rotation_sounds);
     static void config_enable_config(std::string enable_config);
     static void config_boot_mode(std::string boot_mode);
-    static void config_serial(std::string port, std::string command, std::string proceed); 
+    static void config_serial(std::string port, std::string command, std::string proceed);
 
 public:
     static char * url_decode(char * dst, const char * src, size_t dstsize);
