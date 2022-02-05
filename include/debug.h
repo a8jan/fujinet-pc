8572da67e44f576bbaa795fae169ef36dd6c8df2
Jan Krupa <apc.atari@gmail.com>
2022-02-05 19:37:53 +0100

    tweak release build

--- include/debug.h
+++ include/debug.h
@@ -3,24 +3,23 @@
 
 #ifdef __PC_BUILD_DEBUG__
 #define DEBUG
+#endif
 
 #ifdef UNIT_TESTS
 #undef DEBUG
 #endif
 
+#if defined(DEBUG) || !defined(NO_DEBUG_PRINT)
 #include <utils.h>
 /*
   Debugging Macros
 */
-    // Use standard printf
     #define Debug_print(...) util_debug_printf(nullptr, __VA_ARGS__)
     #define Debug_printf(...) util_debug_printf(__VA_ARGS__)
     #define Debug_println(...) util_debug_printf("%s\n", __VA_ARGS__)
 
     #define HEAP_CHECK(x) Debug_printf("HEAP CHECK %s " x "\n", true ? "PASSED":"FAILED")
-#endif
-
-#ifndef DEBUG
+#else
     #define Debug_print(...)
     #define Debug_printf(...)
     #define Debug_println(...)
