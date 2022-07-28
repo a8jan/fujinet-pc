e7d020c4053984d1535db4ff2dbc0bd52937551d
Jan Krupa <apc.atari@gmail.com>
2022-07-28 03:02:12 +0200

    update libsmb2

--- components/libsmb2/include/libsmb2-private.h
+++ components/libsmb2/include/libsmb2-private.h
@@ -23,7 +23,7 @@
 #include "config.h"
 #endif
 
-#if defined(PS2_EE_PLATFORM) || defined(PS3_PPU_PLATFORM) || defined(ESP_PLATFORM) || defined(__APPLE__)
+#if defined(PS2_EE_PLATFORM) || defined(PS3_PPU_PLATFORM) || defined(ESP_PLATFORM) || defined(__APPLE__) || defined(PS4_PLATFORM)
 /* We need this for time_t */
 #include <time.h>
 #endif
@@ -148,6 +148,8 @@ struct smb2_context {
         const char *workstation;
         char client_challenge[8];
 
+        void *opaque;
+
         smb2_command_cb connect_cb;
         void *connect_data;
 
@@ -266,21 +268,21 @@ struct smb2_pdu {
         time_t timeout;
 };
 
-/* UCS2 is always in Little Endianness */
-struct ucs2 {
+/* SMB's UTF-16 is always in Little Endian */
+struct utf16 {
         int len;
         uint16_t val[1];
 };
 
-/* Returns a string converted to UCS2 format. Use free() to release
- * the ucs2 string.
+/* Returns a string converted to UTF-16 format. Use free() to release
+ * the utf16 string.
  */
-struct ucs2 *utf8_to_ucs2(const char *utf8);
+struct utf16 *utf8_to_utf16(const char *utf8);
         
 /* Returns a string converted to UTF8 format. Use free() to release
  * the utf8 string.
  */
-const char *ucs2_to_utf8(const uint16_t *str, int len);
+const char *utf16_to_utf8(const uint16_t *str, int len);
 
 /* Convert a win timestamp to a unix timeval */
 void win_to_timeval(uint64_t smb2_time, struct smb2_timeval *tv);
